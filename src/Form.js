642a9b3a198e751773c93a2cba2397aed02aae50
Anupras Mohapatra <amohapatra3@wisc.edu>
2021-07-24 19:07:16 +0400

    separating form and conversion result into different components

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Conversion from "./Conversion";
 const API_KEY = "f08725edef39eafa2ef31749";
 const url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/USD";
 function Form() {
   const [countries, setCountries] = useState({});
-  // const [changeFrom, setChangeFrom] = useState("");
-  // const [changeTo, setChangeTo] = useState("");
-  // const [amount, setAmount] = useState("");
-  // const [convertedAmount, setConvertedAmount] = useState("s");
   const [values, setValues] = useState({
     changeFrom: " ",
     changeTo: " ",
@@ -76,49 +72,47 @@ function Form() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label> Amount</label>
-      <input type="text" value={values.amount} onChange={setAmount}></input>
-      <label> From </label>
-      <select
-        name="changeFrom"
-        value={values.changeFrom}
-        placeholder="currency from"
-        onChange={setChangeFrom}
-        required
-        aria-invalid="false"
-      >
-        <option value="" selected>
-          Please Select
-        </option>
-        {countryList}
-      </select>
-      <label> To </label>
-      <select
-        name="changeTo"
-        value={values.changeTo}
-        onChange={setChangeTo}
-        required
-        aria-invalid="false"
-      >
-        <option value="" selected>
-          Please Select
-        </option>
-        {countryList}
-      </select>
-      <button> Convert</button>
-      {submit ? (
-        <div>
-          {values.changeFrom +
-            " " +
-            values.amount +
-            " = " +
-            values.changeTo +
-            " " +
-            convertedAmount}
-        </div>
-      ) : null}
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label> Amount</label>
+        <input type="text" value={values.amount} onChange={setAmount}></input>
+        <label> From </label>
+        <select
+          name="changeFrom"
+          value={values.changeFrom}
+          placeholder="currency from"
+          onChange={setChangeFrom}
+          required
+          aria-invalid="false"
+        >
+          <option value="" selected>
+            Please Select
+          </option>
+          {countryList}
+        </select>
+        <label> To </label>
+        <select
+          name="changeTo"
+          value={values.changeTo}
+          onChange={setChangeTo}
+          required
+          aria-invalid="false"
+        >
+          <option value="" selected>
+            Please Select
+          </option>
+          {countryList}
+        </select>
+        <button> Convert</button>
+      </form>
+      <Conversion
+        changeFrom={values.changeFrom}
+        amount={values.amount}
+        changeTo={values.changeTo}
+        submit={submit}
+        convertedAmount={convertedAmount}
+      />
+    </>
   );
 }
 
